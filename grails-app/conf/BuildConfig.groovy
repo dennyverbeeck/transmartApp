979116f4d2a817c966ee570c9c847751ecca2529
dennyverbeeck <dennyverbeeck@gmail.com>
2017-07-03 16:38:57 +0200

    Swap out SmartR version 1.3-DSI for 1.2-STABLE, since 1.3-DSI is not on a public platform yet.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -135,7 +135,7 @@ grails.project.dependency.resolution = {
         compile ":codenarc:0.21"
 
         if (!dm) {
-            runtime ':smart-r:1.3-DSI'
+            runtime ':smart-r:1.2-STABLE-SNAPSHOT'
             compile ':rdc-rmodules:16.2'
             runtime ':transmart-core:16.2'
             compile ':transmart-gwas:16.2'
